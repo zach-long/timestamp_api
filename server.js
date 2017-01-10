2cddf829e434a639b90155254096a25e2a174ecc
Zach <trombonezach@gmail.com>
2017-01-10 10:55:13 -0500

    Added index page

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ var message = 'Server listening on port ' + port + ' . . .'
 
 var welcomeMessage = "Welcome. If you're accessing this directly instead of utilzing it as an API, just append a date to the root url to verify and convert it."
 
-// set default root handling
+// set default root response
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs')
+
 app.get('/', (request, response) => {
-	response.send(welcomeMessage)
+	response.render('index.html')
 })
 
 // import 'timestamp.js' logic
