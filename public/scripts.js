3cb54b33dd36ebd50e2294155782d13714f54091
Zach <trombonezach@gmail.com>
2017-01-11 10:06:18 -0500

    http request works now

--- public/scripts.js
+++ public/scripts.js
@@ -1,24 +1,23 @@
-//'use strict'
+/* function which makes an http request to the server
+   to provide an example of the API in real-time
+   based on user input */
+function httpRequest() {
+  // HTML location to append result
+  var dataAnchor = document.getElementById('live-sample');
+  // user input
+  var requestData = document.getElementById('request-field').value;
+  // the url where the request is being made
+  var url = 'https://fierce-beyond-84088.herokuapp.com/';
+  // concatenation of base url and user request
+  var requestUrl = url + requestData;
 
-console.log("Client side script loaded");
-
-// DOM location to append XMLHttp result
-var dataAnchor = document.getElementById('live-sample');
-// URL for the API call
-var url = 'https://fierce-beyond-84088.herokuapp.com/whoami';
-// error message
-var errMsg = "Unable to retrieve data";
-
-// the HTTP request
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    console.log("request successful");
-    dataAnchor.innerHTML = xhr.responseText;
-  } else {
-    console.log("request failed");
-    dataAnchor.innerHTML = errMsg;
-  }
+  // the http request
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      dataAnchor.innerHTML = xhr.responseText;
+    }
+  };
+  xhr.open('GET', requestUrl, true);
+  xhr.send(null);
 };
-xhr.open('GET', url, true);
-xhr.send(null);
